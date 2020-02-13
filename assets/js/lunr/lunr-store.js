01ab825a7c81df22ca0a4301ceded4d064ca66be
zeroLR <ceyanglab@gmail.com>
2020-02-13 19:58:09 +0800

    Update

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
-        "title": "標題helloworld",
-        "excerpt":"第一篇blog的內文  ","categories": ["分類"],
+        "title": "Pytorch安裝(使用Anaconda 3)",
+        "excerpt":"Pytorch很適合想入門深度學習的人使用，官方文檔整理的很清楚，社區雖然還不是很完善，但有很多相關的教程可以幫助學習，之後再開一篇學習資源彙整，這篇先把Pytorch的環境架好，我是使用Windows10 搭配Anaconda3進行安裝 環境 : Anaconda 3 、 Sublime Text 、 Pytorch 1.4 (Conda,Python,CUDA 10.1) 確認顯卡有無支援CUDA，沒有則使用CPU CUDA支援GPU一覽 安裝步驟 : 安裝Anaconda 3,pytorch,Sublime Text,VsCode 在Anaconda建立虛擬環境(使用python 3.7.6) conda create -n pytorch_GPU python==3.7 切換至pytorch虛擬環境 activate pytorch_GPU 安裝pytorch(with CUDA) conda install pytorch torchvision cudatoolkit=10.1 -c pytorch 安裝pytorch(CPU) conda install pytorch torchvision cpuonly -c pytorch 進python測試是否安裝成功 import...","categories": ["分類"],
         "tags": ["標籤"],
-        "url": "https://zerolr/zeroLR.github.io/%E5%88%86%E9%A1%9E/Hello/",
-        "teaser":null}]
+        "url": "https://zerolr/zeroLR.github.io/%E5%88%86%E9%A1%9E/PytorchInstall/",
+        "teaser":"https://zerolr/zeroLR.github.io/assets/images/z_icon.png"}]
