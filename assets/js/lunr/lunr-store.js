388ade4268c3350556e2f5c66e6c9f0f4738ee65
zeroLR <ceyanglab@gmail.com>
2020-02-15 23:12:58 +0800

    Update

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,11 +1,11 @@
 var store = [{
         "title": "PyTorch安裝(使用Anaconda 3)",
-        "excerpt":"Pytorch很適合想入門深度學習的人使用，官方文檔整理的很清楚，社區雖然還不是很完善，但有很多相關的教程可以幫助學習，之後再開一篇學習資源彙整，這篇先把Pytorch的環境架好，我是使用Win10搭配Anaconda3進行安裝 環境 : Anaconda 3 、 Sublime Text 、 Pytorch 1.4 (Conda,Python,CUDA 10.1) ⚠ 確認顯卡有無支援CUDA，沒有則使用CPU CUDA支援GPU一覽 安裝 Anaconda &amp; Pytorch 安裝Anaconda 3,pytorch,Sublime Text,VsCode(後兩者擇一) 在Anaconda建立虛擬環境(使用python 3.7.6) conda create -n pytorch_GPU python==3.7 切換至pytorch虛擬環境 activate pytorch_GPU 安裝pytorch(with CUDA) conda install pytorch torchvision cudatoolkit=10.1 -c pytorch 安裝pytorch(CPU) &lt;- 顯卡不支援CUDA時用這個 conda install pytorch torchvision cpuonly...","categories": ["Tools"],
+        "excerpt":"Pytorch很適合想入門深度學習的人使用，官方文檔整理的很清楚，社區雖然還不是很完善，但有很多相關的教程可以幫助學習，之後再開一篇學習資源彙整，這篇先把Pytorch的環境架好，我是使用Win10搭配Anaconda3進行安裝 環境 : Anaconda 3 、 Sublime Text 、 Pytorch 1.4 (Conda,Python,CUDA 10.1) ⚠   確認顯卡有無支援CUDA，沒有則使用CPU CUDA支援GPU一覽 安裝 Anaconda &amp; Pytorch 安裝Anaconda 3,pytorch,Sublime Text,VsCode(後兩者擇一) 在Anaconda建立虛擬環境(使用python 3.7.6) conda create -n pytorch_GPU python==3.7 切換至pytorch虛擬環境 activate pytorch_GPU 安裝pytorch(with CUDA) conda install pytorch torchvision cudatoolkit=10.1 -c pytorch 安裝pytorch(CPU) &lt;- 顯卡不支援CUDA時用這個 conda install pytorch torchvision...","categories": ["Tools"],
         "tags": ["Pytorch","Anaconda","Sublime Text","VsCode"],
         "url": "https://zerolr.github.io/tools/PytorchInstall/",
         "teaser":"https://zerolr.github.io/assets/images/PytorchInstall/pytorch.png"},{
         "title": "學習資源",
-        "excerpt":"測試頁  ","categories": ["Resources"],
-        "tags": ["Pytorch","Anaconda","Sublime Text","VsCode"],
+        "excerpt":"PyTorch     這系列教學可以快速入門PyTorch，從安裝到建造神經網絡、CNN、RNN、DQN等等，各種淺顯易懂的說明，包含影片、文章、和所有代碼。   Pytorch 教程系列 / 莫煩Python     PyTorch官方文檔，包含官方教學、社區、Github，都在這網站裡，基本上看完莫煩有個初步了解後可以開始嗑官方教程了。   PyTorch官方文檔     PyTorch中文文檔，不過我個人偏向看官方英文文檔，有時看英文反而比較好理解，除非看不懂再來這尋暖。   PyTorch中文文檔     從初階到實戰，各種大大小小的學習資源全部都在這啦，目前還沒開始研究，但嗑完這個…除了巨佬我想不到別種稱號了。   史上最全的PyTorch學習資源匯總   數據處理     兩個科學運算當中最為重要的兩個模塊，應用在數據分析、機器學習、深度學習，運算速度快且消耗資源少。   Numpy &amp; Pandas 教程系列 / 莫煩Python     對於需要大量數據的機器學習來說，爬蟲是很好的技術，可以省下許多收集資料的時間，讓作者莫煩帶你認識和實作爬蟲吧。   網頁爬蟲教程系列 / 莫煩Python   Python     從安裝開始到各種變數、函數運算、model導入、文件讀取等Python的基礎教學，適合未接觸過或是想複習的朋友們。   基礎教程系列 / 莫煩Python     包含基礎python、MySQL、MongoDB教學，提供範例、練習、測驗及線上考證，可以好好確認自己的實力。   w3schools Python...","categories": ["Resources"],
+        "tags": ["Pytorch","Numpy","Pandas","Python","GitHub","Papers"],
         "url": "https://zerolr.github.io/resources/Resources/",
         "teaser":"https://zerolr.github.io/assets/images/PytorchInstall/pytorch.png"}]
