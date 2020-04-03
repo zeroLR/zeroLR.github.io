5811ed3049bc462aac19f267b10f2c27265657d3
zeroLR <ceyanglab@gmail.com>
2020-04-03 20:29:19 +0800

    Update

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -8,4 +8,9 @@ var store = [{
         "excerpt":"這邊是我個人目前有在關注的學習資源，若有敘述錯誤或是連結失效請留言或email，有推薦的文章或教程也歡迎推坑，先感謝各位大大的指教了。 PyTorch     這系列教學可以快速入門PyTorch，從安裝到建造神經網絡、CNN、RNN、DQN等等，各種淺顯易懂的說明，包含影片、文章、和所有代碼。   Pytorch 教程系列 / 莫煩Python     PyTorch官方文檔，包含官方教學、社區、Github，都在這網站裡，基本上看完莫煩有個初步了解後可以開始嗑官方教程了。   PyTorch官方文檔     PyTorch中文文檔，不過我個人偏向看官方英文文檔，有時看英文反而比較好理解，除非看不懂再來這尋暖。   PyTorch中文文檔     從初階到實戰，各種大大小小的學習資源全部都在這啦，目前還沒開始研究，但嗑完這個的話…JoJo我不做人啦!!!   史上最全的PyTorch學習資源匯總   數據處理     兩個科學運算當中最為重要的兩個模塊，應用在數據分析、機器學習、深度學習，運算速度快且消耗資源少。   Numpy &amp; Pandas 教程系列 / 莫煩Python     對於需要大量數據的機器學習來說，爬蟲是很好的技術，可以省下許多收集資料的時間，讓作者莫煩帶你認識和實作爬蟲吧。   網頁爬蟲教程系列 / 莫煩Python   DL/ML/RL     這系列帶你認識一些常見的神經網絡和強化學習的相關知識，在動手實作前不妨看一看，可以對機器學習有更多認識喔。   有趣的機器學習系列 / 莫煩Python     對強化學習有認識後，這系列教程會實作各種model，必須具備Numpy和Pandas的知識才比較好上手，教程使用Tensorflow來實作。   強化學習系列教程...","categories": ["Resources"],
         "tags": ["Pytorch","Numpy","Pandas","Python","GitHub","Papers"],
         "url": "https://zerolr.github.io/resources/Resources/",
-        "teaser":"https://zerolr.github.io/assets/images/PytorchInstall/pytorch.png"}]
+        "teaser":"https://zerolr.github.io/assets/images/PytorchInstall/pytorch.png"},{
+        "title": "轉貼集中地",
+        "excerpt":"   記錄一下優質的文章    Python          感謝作者Clay所寫的這篇文章，最近要做的東西剛好需要用到，平常用來下載Youtube的影片也非常方便!    Python 當中使用 pytube 下載 Youtube 的影片        ","categories": ["Share"],
+        "tags": ["Python","Youtube"],
+        "url": "https://zerolr.github.io/share/Share/",
+        "teaser":"https://images.unsplash.com/photo-1482066490729-6f26115b60dc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1302&q=80"}]
