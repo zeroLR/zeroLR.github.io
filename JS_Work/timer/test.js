ce656fea358ff77cbda950e57046c20951e3b1fe
zeroLR <60684158+zeroLR@users.noreply.github.com>
2020-06-24 23:51:29 +0800

    Update

--- JS_Work/timer/test.js
+++ JS_Work/timer/test.js
@@ -12,20 +12,20 @@ function addckbox(boxname, prompt, ckfunc) {
   return str;
 }
 
-var row = document.getElementById("SCHE");
-row = row.insertRow(row.rows.length - 1);
-row.insertCell(0).innerHTML =
-  "<input type='button' value='儲存' onclick='appSche(1)'><input type='button' value='取消' onclick='appSche(0)'>";
-row.insertCell(0).innerHTML = "<input type='number' id='v3' value='0'>";
-row.insertCell(0).innerHTML = "<input type='number' id='v2' value='0'>";
-row.insertCell(0).innerHTML = "<input type='number' id='v1'value='0'>";
-row.insertCell(0).innerHTML = "<input type='time' id='tm'>";
-str = addckbox("d0", "每一天");
-str += addckbox("w0", "星期日");
-str += addckbox("w1", "星期一");
-str += addckbox("w2", "星期二");
-str += addckbox("w3", "星期三");
-str += addckbox("w4", "星期四");
-str += addckbox("w5", "星期五");
-str += addckbox("w6", "星期六");
-row.insertCell(0).innerHTML = str;
+// var row = document.getElementById("SCHE");
+// row = row.insertRow(row.rows.length - 1);
+// row.insertCell(0).innerHTML =
+//   "<input type='button' value='儲存' onclick='appSche(1)'><input type='button' value='取消' onclick='appSche(0)'>";
+// row.insertCell(0).innerHTML = "<input type='number' id='v3' value='0'>";
+// row.insertCell(0).innerHTML = "<input type='number' id='v2' value='0'>";
+// row.insertCell(0).innerHTML = "<input type='number' id='v1'value='0'>";
+// row.insertCell(0).innerHTML = "<input type='time' id='tm'>";
+// str = addckbox("d0", "每一天");
+// str += addckbox("w0", "星期日");
+// str += addckbox("w1", "星期一");
+// str += addckbox("w2", "星期二");
+// str += addckbox("w3", "星期三");
+// str += addckbox("w4", "星期四");
+// str += addckbox("w5", "星期五");
+// str += addckbox("w6", "星期六");
+// row.insertCell(0).innerHTML = str;
