b59c099e50af476c5a3ab7c1b1c107920a42a01f
zerolr <ceyang@gmail.com>
2020-08-07 18:02:53 +0800

    update

---
+++ JS_Work/All_Project/elevator/function.js
@@ -0,0 +1,43 @@
+function motorSW(state) {
+  switch (state) {
+    case "lock":
+      console.log("鎖住馬達");
+    case "unlock":
+      console.log("解鎖馬達");
+  }
+}
+
+function motorACT(state) {
+  switch (state) {
+    case "up":
+      console.log("開始向上移動");
+    case "down":
+      console.log("開始向下移動");
+    case "stop":
+      console.log("停止動作");
+    case "speedUp":
+      console.log("加速");
+    case "speedDown:":
+      console.log("減速");
+  }
+}
+
+function posSetStart() {
+  console.log("設定起始點");
+}
+function posSetEnd() {
+  console.log("設定終止點");
+}
+
+function test() {
+  console.log("測試動作");
+}
+
+function loopACT(state) {
+  switch (state) {
+    case "start":
+      console.log("循環啟動");
+    case "stop":
+      console.log("循環停止");
+  }
+}
