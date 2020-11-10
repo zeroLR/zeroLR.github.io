c7b8ceeb163daa5244721d36e00ac86e18180821
楊承恩 <10411222@gm.nttu.edu.tw>
2020-11-10 16:00:58 +0800

    update:pwa

---
+++ JS_Work/pwa/precache-manifest.e1726910a6cad16ede11f83b9ed9b8a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb40336af6312a3f807e7bcc624bd4e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30d6ee27bd24bb321b44",
+    "url": "/static/css/main.36d11973.chunk.css"
+  },
+  {
+    "revision": "4674e7dd751465f5a74d",
+    "url": "/static/js/2.e78d4dff.chunk.js"
+  },
+  {
+    "revision": "30d6ee27bd24bb321b44",
+    "url": "/static/js/main.18dcbbf1.chunk.js"
+  },
+  {
+    "revision": "a2994b69f21c76da2525",
+    "url": "/static/js/runtime~main.5ece7818.js"
+  }
+]);
