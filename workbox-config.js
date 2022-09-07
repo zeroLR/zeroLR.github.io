959be8e35db447cbc9192cd7eba017c916a99055
ceyang <ceyanglab@gmail.com>
2022-09-07 16:44:51 +0800

    feat: update service worker

--- workbox-config.js
+++ workbox-config.js
@@ -1,11 +1,6 @@
 module.exports = {
-	globDirectory: 'public/',
-	globPatterns: [
-		'**/*.{html,yml,jpg,png,json,moc,mtn,js,xml,ico,svg,jpeg,txt,css}'
-	],
-	swDest: 'static/sw.js',
-	ignoreURLParametersMatching: [
-		/^utm_/,
-		/^fbclid$/
-	]
+  globDirectory: "public/",
+  globPatterns: ["**/*.{html,yml,json,moc,mtn,js,xml,ico,svg,txt,css}"],
+  swDest: "static/sw.js",
+  ignoreURLParametersMatching: [/^utm_/, /^fbclid$/, /^about/],
 };
